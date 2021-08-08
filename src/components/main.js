4b6e75334ada58b2725902dc88b27e077d1bc836
duajaradat <duajaradat164@gmail.com>
2021-08-08 18:51:09 +0300

    solve lab01/components

---
+++ src/components/main.js
@@ -0,0 +1,14 @@
+import React from "react";
+import HornedBeasts from "./HornedBeasts";
+
+class Main extends React.Component {
+  render() {
+    return (
+      <div>
+        <HornedBeasts />
+       
+      </div>
+    );
+  }
+}
+export default Main;
