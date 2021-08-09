33c8119990f7bcced059fe340e06b497395d607b
duajaradat <duajaradat164@gmail.com>
2021-08-09 21:20:41 +0300

    lab02 (props/state/bootstrap) react solved

--- src/components/HornedBeasts.js
+++ src/components/HornedBeasts.js
@@ -1,55 +1,40 @@
 /* eslint-disable eol-last */
 
 import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
 
 class HornedBeasts extends React.Component {
-
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state={
-      votes : 0
+    this.state = {
+      like: 0,
     };
   }
-  votesCounter =()=>{
+  likesCounter = () => {
     this.setState({
-      votes:this.state.votes+1
+      like: this.state.like + 1,
     });
-  }
-  
+  };
+
   render() {
     return (
       <div>
-        <h2>{this.props.title}</h2>
-        <img onClick={this.votesCounter}
-          src={this.props.img}
-          title={this.props.title}
-          alt={this.props.title}
-        />
-        <p>{this.props.description}</p>
-        <p>Votes:{this.state.votes} ❤ </p>
+        <Card style={{ width: "18rem" }}>
+          <Card.Img  src={this.props.img} />
+          <Card.Body>
+            <Card.Title>{this.props.title}</Card.Title>
+            <Card.Text>{this.props.description}</Card.Text>
+            <Card.Text>Likes {this.state.like} ❤ </Card.Text>
+            <Button onClick={this.likesCounter}>Like</Button>
+          </Card.Body>
+        </Card>
       </div>
     );
   }
 }
 export default HornedBeasts;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 ///////////////////////////////////////////////
 // class HornedBeasts extends React.Component {
@@ -65,7 +50,7 @@ export default HornedBeasts;
 //       votes:this.state.votes+1
 //     });
 //   }
-  
+
 //   render() {
 //     return (
 //       <div>
