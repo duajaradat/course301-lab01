13c8469fb5c2a493f65f8490cb04af620513fae1
duajaradat <duajaradat164@gmail.com>
2021-08-10 20:00:53 +0300

    add new component

--- src/App.js
+++ src/App.js
@@ -2,14 +2,34 @@ import React from "react";
 import Header from "./components/header";
 import Footer from "./components/footer";
 import Main from "./components/main";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Imgdata from "./components/imgdata.json";
+import SelectedBeast  from "./components/SelectedBeast";
+
 
 
 class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+      show: false
+    };
+  }
+  // shoPopUp=()=>{
+  //   Imgdata.find((element=>{
+  //     if(element == this.props.)
+  //   }))
+  //   this.setState({
+  //   show:
+  //   })
+  // }
+
   render() {
     return (
       <div>
         <Header />
-        <Main />
+        <Main imgData={Imgdata}/>
+        <SelectedBeast />
         <Footer />
       </div>
     );
